c1ee3fb84919c718bdd50292a89c8a3cfbe9e8d0
amaentau <amaentau@yahoo.com>
2026-01-02 21:47:23 +0200

    Top level cleanup

--- deploy_stage/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send('Hello World - Deployment Test'));
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
